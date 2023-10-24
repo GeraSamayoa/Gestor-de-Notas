eec6ad91f358585fdd4459c8ac7c747f2dece763
mybg0 <mbarrerag5@miumg.edu.gt>
2023-10-23 22:34:29 -0600

    BIENVENIDA FORM

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -16,7 +16,7 @@ namespace Frontend
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BIENVENIDA());
         }
     }
 }
